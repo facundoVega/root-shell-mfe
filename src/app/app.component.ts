fcbd4ebadf55bd6349713b369c2c8ac01b0a33c5
Horacio Vega <2187815@corp.trizetto.com>
2024-06-28 17:13:09 -0300

    Store flow added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Component,  OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { MsalCustomService } from '@mezomon/shared-library-test';
+import { MsalCustomService, activeUSer, currentUser } from '@mezomon/shared-library-test';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,24 @@ import { MsalCustomService } from '@mezomon/shared-library-test';
 })
 export class AppComponent implements OnInit  {
   user!: string;
+
+  $user = this.store.select(activeUSer)
   @ViewChild("container", { read: ViewContainerRef }) mainContainer!: ViewContainerRef;
   @ViewChild("footerContainer", { read: ViewContainerRef }) footerContainer!: ViewContainerRef;
 
   constructor(
-    private msalCustomService: MsalCustomService
+    private msalCustomService: MsalCustomService,
+    private store: Store,
   ) {}
 
   
   ngOnInit(): void {
+    this.loadFooter();
+
     this.msalCustomService.checkLogin().subscribe((userName)=> { 
       this.user = userName;
+      this.store.dispatch(currentUser({userName: this.user}));
     });
-    this.loadFooter();
   }
 
   logout(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table-component/table.component';
 import { MsalRedirectComponent } from '@azure/msal-angular';
-import { msalAuthModule, HeaderComponent, SidebarComponent } from '@mezomon/shared-library-test';
+import { msalAuthModule, HeaderComponent, getStoreModule, SidebarComponent } from '@mezomon/shared-library-test';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,13 @@ import { msalAuthModule, HeaderComponent, SidebarComponent } from '@mezomon/shar
     AppRoutingModule,
     msalAuthModule,
     HeaderComponent,
-    SidebarComponent
+    getStoreModule(),
+    SidebarComponent,
+    StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: false,
+        connectInZone: true,
+      }),
   ],
   providers: [],
   bootstrap: [AppComponent, MsalRedirectComponent]
